42b8271411a381fc893356ee79426ddb7e788131
Rahul <rajrahul00945@gmail.com>
2025-03-02 02:09:37 +0530

    bug fix and logo added

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ const v1Router = require("./routes/v1/v1Router");
 const cors = require("cors");
 
 const NODE_ENV = process.env.NODE_ENV
-const PORT = process.env[`${NODE_ENV}_PORT`]  || 8080
+const PORT = process.env[`${NODE_ENV}_PORT`]  || 4000 ;
 
 const server = express();
 
